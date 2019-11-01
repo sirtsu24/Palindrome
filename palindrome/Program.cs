cc59cb909d1add20551ccedcc0db1663c0b889e9
Sirje S <sirtsu24@hotmail.com>
2019-11-01 13:04:11 +0200

    Palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAnswer;
+
+            Console.WriteLine("Enter your name: ");
+            userAnswer = Console.ReadLine();
+
+            ReversedName(userAnswer);
+           
+
+            if (userAnswer.ToLower() == ReversedName(userAnswer).ToLower())
+            {
+                Console.WriteLine("It is a palindrome");
+            }
+
+            else
+            {
+                Console.WriteLine("It is not a palindrome");
+            }
+
+
+        }
+        public static string ReversedName(string s)
+        {
+            char[] name = s.ToCharArray();
+            Array.Reverse(name);
+            return new string(name);
+                      
+        }
+    }
+    }
